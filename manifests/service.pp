6881cb0c87dd2d55c6cf5f4cd0671ba1342516bd
Luan Cestari <luan.cestari@gmail.com>
2014-05-06 00:11:12 -0300

    Fix

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,13 +2,10 @@
 # This class ensures the service is enabled and running.
 #
 class jboss_as::service {
-
   include jboss_as::config
-
   service { 'jboss-as':
     ensure     => running,
     enable     => true,
-    require    => File['/etc/jboss-as/jboss-as.conf'],
     subscribe  => File["${jboss_as::jboss_home}/standalone/configuration/standalone.xml"]
   }
 }
